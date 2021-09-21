1b303c62ffb13a7f524f79a30fae856d788fd879
DylanToporek <dylantoporek@gmail.com>
2021-09-21 17:50:17 -0400

    commit tree 802cbbc686aee0be5c1109eacdc1565826e9dda4

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function saturdayFun(activity){
+    activity = (typeof activity !== 'undefined') ?  activity : 'roller-skate';
+    return (`This Saturday, I want to ${activity}!`);
+    
+}
+
+function mondayWork(plans){
+    plans = (typeof plans !== 'undefined') ? plans : 'go to the office';
+    return (`This Monday, I will ${plans}.`);
+}
+
+function wrapAdjective(adj){
+    adj = (typeof adj !== 'undefined') ? adj : '*';
+    const part1 = 'You are';
+    return function(string){
+      return (`${part1} ${adj}${string}${adj}!`);
+    };
+     
+  }
